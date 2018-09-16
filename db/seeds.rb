4d682f0928b3b749edaf64b2cb1eccaa4af6d415
Jai <loveyhtu21@gmail.com>
2018-09-16 17:18:06 +0530

    require testing in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 require "faker"
-
 # Create users with random IP addresses, for use in demonstrating tests
 # involving external HTTP services.
 20.times do
